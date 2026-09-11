74984e624c21a04598ff246ccc28de6862ba6e70
beauchampsteve-lab <beauchamp.steve@gmail.com>
2026-09-11 14:25:04 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-const CACHE='workout-v8';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png']))) });
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(new URL(e.request.url).pathname.endsWith('/config.js')) return; e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))) });
+const CACHE = 'workout-v7';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k.startsWith('workout-v') && k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
